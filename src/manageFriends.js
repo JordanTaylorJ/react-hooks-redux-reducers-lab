3bad0e3dcc4ded949a1e2ef49281a048a039f0d8
JordanJoseph <jordant.joseph@outlook.com>
2023-01-26 10:30:13 -0700

    commit tree 936d00a6765f59b980ca29708e97f8ec071b7e30

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
 export function manageFriends(state, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {...state.friends, action}
+      //{friends: state.friends.push(action.payload)}
+    case "friends/remove":
+      return state.friends.filter(action.payload)
+    default: 
+      return state;
+  }
 }
